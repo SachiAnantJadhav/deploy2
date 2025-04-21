0b2ed8ccf9463e270b8abbab9093c5dab7de2e6e
SachiAnantJadhav <sachijadhav2125@gmail.com>
2025-04-21 09:50:10 +0530

    second commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import App from './App';
+// import './index.css';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -9,3 +24,21 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+
+// ✅ Register service worker
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/deploy2/service-worker.js')
+      .then((reg) => {
+        console.log('✅ Service Worker registered: ', reg);
+      })
+      .catch((err) => {
+        console.log('❌ Service Worker registration failed: ', err);
+      });
+  });
+}
+
+
+reportWebVitals();
